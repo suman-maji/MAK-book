196429ede6a8e1fa03e35c3557d04688386d76db
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2025-02-20 11:34:20 +0530

    Update Faqs.jsx

--- src/Component/Faqs/Faqs.jsx
+++ src/Component/Faqs/Faqs.jsx
@@ -1,43 +1,38 @@
-import React from 'react'
-import Fag from './Faq'
+import React from 'react';
+import Fag from './Faq';
 
 const Faqs = () => {
     const faqs = [
         {
-            Ques: `
-            What subjects does this website cover? `,
-            Ans: `Our website provides study resources for a comprehensive range of subjects relevant to your MAKAUT University stream. Be sure to explore the "Subjects" section to find the specific topics you need`
+            Ques: `What subjects does this website cover?`,
+            Ans: `Our website provides study resources for a comprehensive range of subjects relevant to your MAKAUT University stream. Be sure to explore the "Subjects" section to find the specific topics you need.`
         },
         {
-            Ques: `Are the previous year question papers downloadable or can I only view them online? `,
-            Ans: ` We offer flexibility in accessing past exam papers. You can download them for offline study using Google Drive or conveniently view them directly on our website.`
+            Ques: `Are the previous year question papers downloadable or can I only view them online?`,
+            Ans: `We offer flexibility in accessing past exam papers. You can download them for offline study using Google Drive or conveniently view them directly on our website.`
         },
         {
-            Ques: ` Do you offer any answer keys or explanations for the previous year questions?`,
-            Ans: ` We understand the importance of answer keys and explanations for effective exam preparation. While we are currently developing this feature and it will be available soon, you'll find solutions for many questions already included within the downloadable subject organizers`
+            Ques: `Do you offer any answer keys or explanations for the previous year questions?`,
+            Ans: `We understand the importance of answer keys and explanations for effective exam preparation. While we are currently developing this feature and it will be available soon, you'll find solutions for many questions already included within the downloadable subject organizers.`
         },
         {
-            Ques: ` Is this website free to use? `,
-            Ans: `Access to our website's resources requires a time investment from you, allowing you to reap the benefits of focused exam preparation.`
+            Ques: `Is this website free to use?`,
+            Ans: `Access to our website's resources is completely free. We aim to provide valuable study materials to help you excel in your academics.`
         },
-    ]
+    ];
+    
     return (
-        <div
-            className='bg-gradient-to-tr pt-6 from-indigo-900/50 via-gray-800 to-indigo-900 top-0 text-white py-10  p-2 sm:p-4 '>
-            <h1 className='text-3xl sm:text-5xl p-4 sm:p-6 font-bold text-center'>Frequently Asked Questions</h1>
-            <div className='flex flex-col gap-3'>
+        <div className='bg-gradient-to-tr from-indigo-900/50 via-gray-800 to-indigo-900 text-white py-16 px-4 sm:px-8 rounded-lg shadow-lg'>
+            <h1 className='text-4xl sm:text-6xl font-extrabold text-center mb-8'>Frequently Asked Questions</h1>
+            <div className='flex flex-col gap-6'>
                 {
-                    faqs.map((elem, ind) => {
-                        return (
-                            <Fag key={ind} elem={elem} />
-                        )
-                    })
+                    faqs.map((elem, ind) => (
+                        <Fag key={ind} elem={elem} />
+                    ))
                 }
             </div>
-            <br />
-
         </div>
-    )
+    );
 }
 
-export default Faqs
+export default Faqs;
