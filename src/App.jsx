bfa6b54a625f1abd28e994ff3de600c0f4178845
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2025-02-20 10:45:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Helmet } from 'react-helmet-async';
 import About from "./Pages/About";
 import Course from "./Pages/Course";
@@ -13,26 +13,25 @@ import DonateDetails from "./Component/Donate/DonateDetails";
 function App() {
   return (
     <>
-      <div className=" bg-gray-800 font-mont">
-          <Helmet>
-            <meta name="description" content="MAKBook - Get the latest makaut study material" />
-          </Helmet>
+      <div className="bg-gray-800 font-mont">
         <BrowserRouter>
+          <Helmet>
+            <title>Snapstudy - Get the Latest MAKAUT Study Material</title>
+            <meta name="description" content="Snapstudy - Your ultimate resource for the latest MAKAUT study materials, organized notices, and previous year question papers. Simplify your MAKAUT studies with Snapstudy." />
+            <meta name="keywords" content="Snapstudy, MAKAUT, study material, organizer, notice, previous year question papers, resources, exam preparation, students, guides, syllabus" />
+          </Helmet>
           <Routes>
-            <Route path="/" element={
-              <Layout/>
-            }>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/about" element={<About/>}/>
-              <Route path="/contact" element={<Contact/>}/>
-              <Route path="/services" element={<Services/>}/>
-              <Route path="/donate" element={<DonateDetails/>}/>
-              <Route path="/choice/:id" element={<Course/>}/>
-              <Route path="/choice/:id/:branchId/:semId" element={<Subject />} />
-              <Route path="*" element={<Error/>}/>
-          </Route>     
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="services" element={<Services />} />
+              <Route path="donate" element={<DonateDetails />} />
+              <Route path="choice/:id" element={<Course />} />
+              <Route path="choice/:id/:branchId/:semId" element={<Subject />} />
+              <Route path="*" element={<Error />} />
+            </Route>
           </Routes>
-
         </BrowserRouter>
       </div>
     </>
